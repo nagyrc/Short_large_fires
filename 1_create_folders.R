b51434b11a004e90d3ca611a8345875857c2475e
nagyrc <nagyrc@gmail.com>
2018-01-03 12:01:11 -0700

    changed folders

--- 1_create_folders.R
+++ 1_create_folders.R
@@ -12,7 +12,7 @@ raw_prefix <- file.path(prefix, "raw")
 ecoregion_prefix <- file.path(raw_prefix, "us_eco_l3")
 fpa_prefix <- file.path(raw_prefix, "fpa-fod")
 conus_prefix <- file.path(raw_prefix, "conus")
-bio_prefix <- file.path(raw_prefix, "NBCD_countrywide_biomass_mosaic")
+biomass_prefix <- file.path(raw_prefix, "NBCD_countrywide_biomass_mosaic")
 bps_prefix <- file.path(raw_prefix, "us_130bps")
 ws_prefix <- file.path(raw_prefix, "ws")
 fm_prefix <- file.path(raw_prefix, "fm100")
@@ -26,19 +26,19 @@ bio_crt <- file.path(prefix, "bio")
 biomass_crt <- file.path(bio_crt, "biomass")
 bps_crt <- file.path(bio_crt, "bps")
 climate_crt <- file.path(prefix, "climate")
-ws_crt <- file.path(prefix, "ws")
-fm_crt <- file.path(prefix, "fm")
+ws_crt <- file.path(climate_crt, "ws")
+fm_crt <- file.path(climate_crt, "fm")
 
 us_out <- file.path(conus_crt, "cb_2016_us_state_20m")
 ecoregion_out <- file.path(ecoreg_crt, "us_eco_l3")
 fpa_out <- file.path(fire_crt, "fpa-fod")
 biomass_out <- file.path(biomass_crt, "NBCD_countrywide_biomass_mosaic")
 bps_out <- file.path(bps_crt, "us_130bps")
-ws_out <- file.path(bps_crt, "ws")
-fm_out <- file.path(bps_crt, "fm100")
+ws_out <- file.path(ws_crt, "ws")
+fm_out <- file.path(fm_crt, "fm100")
 
 # Check if directory exists for all variable aggregate outputs, if not then create
-var_dir <- list(prefix, raw_prefix, ecoregion_prefix, fpa_prefix, conus_prefix, bio_prefix, bps_prefix, 
-                bounds_crt, conus_crt, ecoreg_crt, fire_crt, bio_crt, biomass_crt, bps_crt,
-                us_out, ecoregion_out, fpa_out, biomass_out, bps_out)
+var_dir <- list(prefix, raw_prefix, ecoregion_prefix, fpa_prefix, conus_prefix, bio_prefix, bps_prefix, climate_prefix, fm_prefix, ws_prefix,
+                bounds_crt, conus_crt, ecoreg_crt, fire_crt, bio_crt, biomass_crt, bps_crt, climate_crt, ws_crt, fm_crt,
+                us_out, ecoregion_out, fpa_out, biomass_out, bps_out, ws_out, fm_out)
 lapply(var_dir, function(x) if(!dir.exists(x)) dir.create(x, showWarnings = FALSE))
